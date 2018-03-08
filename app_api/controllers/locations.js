35a356c97381e18eb6b1bb2be0f9150442d0d4d3
GermanMartynov <box1612@gmail.com>
2018-03-08 11:12:51 +0300

    конец ch 6.2.2 GET запрос местоположения

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -6,11 +6,29 @@ var sendJsonResponce = function(res, status, content){
     res.json(content);
 };
 
+// GET
+module.exports.locationsReadOne = function (req, res) {
+    if(req.params && req.params.locationid) { // есть ли в параметрах locationid?
+        Loc
+            .findById(req.params.locationid) // получаем из параметров locationid
+            .exec(function (err, location) { // описываем обратный вызов для обработки ответных параметров
+                if(!location) { // если mongoose не вернул местоположение
+                    sendJsonResponce(res, 404, {"message" : "No locationid not found"});
+                    return;
+                } else if (err){ // если mongoose  вернул ошибку
+                    sendJsonResponce(res, 404, err);
+                    return;
+                }
+            sendJsonResponce(res, 200, location);   // Все ок
+            });
+    } else {
+        sendJsonResponce(res, 404, {"message" : "No locationid in request"});
+    }
+};
+
 module.exports.locationsListByDistance = function (req, res) {sendJsonResponce(res, 200, {"status" : "success"})};
 
 module.exports.locationsCreate = function (req, res) {sendJsonResponce(res, 200, {"status" : "success"})};
-
-module.exports.locationsReadOne = function (req, res) {sendJsonResponce(res, 200, {"status" : "success"})};
 
 module.exports.locationsUpdateOne = function (req, res) {sendJsonResponce(res, 200, {"status" : "success"})};
 
